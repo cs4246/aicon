43ae4c6c902db9dfb3934b3c45bc3e3fddf8a983
Muhammad Rizki Aulia Rahman Maulana <rizki@rizkiarm.com>
2024-09-24 15:27:02 +0800

    feat(submission): uncollapse when run is finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,33 @@ $(document).ready(function () {
     event.stopPropagation();
   });
 });
+
+var submissionStatusMap = new Map();
+
+function uncollapseSubmissionIfFinished(_this, event) {
+  for (var elem of event.target.children) {
+    if (!("submissionId" in elem.dataset)) {
+      continue;
+    }
+
+    var submissionId = elem.dataset.submissionId;
+    var submissionStatus = elem.dataset.submissionStatus;
+
+    if (!(submissionId in submissionStatusMap)) {
+      submissionStatusMap[submissionId] = submissionStatus;
+    }
+
+    if (
+      submissionStatus != submissionStatusMap[submissionId] &&
+      ["E", "D"].includes(submissionStatus)
+    ) {
+      var submissionElem = document.getElementById(
+        "submission-" + submissionId,
+      );
+      if (submissionElem.ariaExpanded == "false") {
+        submissionElem.click();
+      }
+    }
+    submissionStatusMap[submissionId] = submissionStatus;
+  }
+}
